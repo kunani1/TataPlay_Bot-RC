59ab31f305de7343a8ba8f6009bb273fee3e8825
kunani1 <131540652+kunani1@users.noreply.github.com>
2024-07-07 16:36:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ api_id = 2929027
 
 api_hash = "2beecc3ee357e6e3f2b2e783d4159f9f"
 
-bot_token = "6397226132:AAHTufPe__SvDAxxql5f8TreLG-9mOXf0Vo"
+bot_token = "1843277705:AAGj9xjvO9jbnC75R8USUKxKjIRmq2k1xlE"
 
 GROUP_TAG = "RC"
 
